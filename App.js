7c040d5ac6afa60eb1d5655b8bdcd973bee0a905
Michael Nguyen <mklnguyen8@gmail.com>
2020-09-24 05:29:31 -0700

    added React Native UI Kitten
    general purpose UI components

--- App.js
+++ App.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import * as eva from '@eva-design/eva';
+import { ApplicationProvider, Layout, Text } from '@ui-kitten/components';
 import {
   SafeAreaView,
   StyleSheet,
@@ -44,4 +46,16 @@ const styles = StyleSheet.create({
     paddingRight: 60,
   }
 });
+
+const HomeScreen = () => (
+  <Layout style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+    <Text category='h1'>HOME</Text>
+  </Layout>
+);
+
+export default () => (
+  <ApplicationProvider {...eva} theme={eva.dark}>
+    <HomeScreen />
+  </ApplicationProvider>
+);
 
