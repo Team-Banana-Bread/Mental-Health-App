0818229e8a06fb54553c513e86d91dc54730b73e
Marlon Aquino <marlon.aquino24@gmail.com>
2020-09-26 16:42:01 -0700

    add folders for components

--- App.js
+++ App.js
@@ -1,9 +1,6 @@
 import React from 'react';
 import * as eva from '@eva-design/eva';
 import { ApplicationProvider, Layout, Text } from '@ui-kitten/components';
-import {
-  StyleSheet,
-} from 'react-native';
 
 export default class App extends React.Component {
 
@@ -17,16 +14,6 @@ export default class App extends React.Component {
 
   } 
 };
-
-const styles = StyleSheet.create({
-  container: {
-    backgroundColor: '#36485f',
-    flex: 1,
-    justifyContent: 'center',
-    paddingLeft: 60, 
-    paddingRight: 60,
-  }
-});
 
 const HomeScreen = () => (
   <Layout style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
