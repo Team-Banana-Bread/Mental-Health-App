f44f0ea33b95ee63c04185aacbe937628f9e9781
Marlon Aquino <marlon.aquino24@gmail.com>
2020-10-27 22:57:36 -0700

    update navigator.js

--- Navigator.js
+++ Navigator.js
@@ -1,20 +1,48 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
+
+import { BottomNavigation, BottomNavigationTab, Layout, Text } from '@ui-kitten/components';
 import { createStackNavigator } from '@react-navigation/stack';
-import { HomeScreen } from './src/components/HomePage/HomePage';
-import { LoginScreen } from './src/components/LoginPage/LoginPage';
+import { createBottomTabNavigator } from'@react-navigation/bottom-tabs';
 
-const { Navigator, Screen } = createStackNavigator();
+import PersonalJournal from './src/components/PersonalJournal/PersonalJournal';
+import GoodNews from './src/components/GoodNewsPage/GoodNews';
 
-const HomeNavigator = () => (
-  <Navigator headerMode='none'>
-    <Screen name='Home' component={HomeScreen}/>
-    <Screen name='Login' component={LoginScreen}/>
+const { Navigator, Screen } = createBottomTabNavigator();
+
+const JournalScreen = () => (
+  <Layout style={{ flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+    <Text category='h1'>JOURNAL</Text>
+  </Layout>
+);
+
+const GoodNewsScreen = () => (
+  <Layout style={{ flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+    <Text category='h1'>GOOD NEWS</Text>
+  </Layout>
+);
+
+const BottomTabBar = ({ navigation, state }) => (
+  <BottomNavigation
+    selectedIndex={state.index}
+    onSelect={index => navigation.navigate(state.routeNames[index])} >
+      <BottomNavigationTab title ='JOURNAL'/>
+      <BottomNavigationTab title ='GOOD NEWS'/>
+    </BottomNavigation>
+);
+
+
+
+const TabNavigator = () => (
+  <Navigator tabBar={props => <BottomTabBar {...props} />}>
+    <Screen name='PersonalJournal' component={PersonalJournal}/>
+    <Screen name='GoodNews' component={GoodNews}/>
   </Navigator>
 );
 
+
 export const AppNavigator = () => (
     <NavigationContainer>
-        <HomeNavigator/>
+      <TabNavigator/>
     </NavigationContainer>
 );
